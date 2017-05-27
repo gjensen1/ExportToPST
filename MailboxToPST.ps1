3e464f701268cde7a1d34a4dfe767427a0e38b3f
gjensen1 <gjensen1@cogeco.ca>
2017-05-27 11:25:13 -0400

    Add files via upload

---
+++ MailboxToPST.ps1
@@ -0,0 +1,34 @@
+﻿
+
+#Define Variables
+
+#RetrieveType is a switch for change from LessThen date, to GreaterThenOrEqualDate.
+$RetrieveType = "First"
+#$RetrieveType = "Second"
+
+$RetrieveDate = '05/01/2017'
+$ExportPath = "\\localhost\exports"
+$UserListFile = "c:\temp\ExportList.txt"
+
+
+
+
+
+#Get MailBox List 
+$UserList = Get-Content $UserListFile
+
+If ($RetrieveType -eq "First"){
+    ForEach ($User in $UserList){
+        New-MailboxExportRequest -ContentFilter {(Received -lt $RetrieveDate)} -Mailbox $User -FilePath $ExportPath\$User-First.pst
+    }
+}Else{
+    ForEach ($User in $UserList){
+        New-MailboxExportRequest -ContentFilter {(Received -ge $RetrieveDate)} -Mailbox $User -FilePath $ExportPath\$User-Second.pst
+    }
+}
+
+
+<#
+New-MailboxExportRequest -ContentFilter {(Received -lt '05/01/2017')}  -Mailbox hearnsa -FilePath \\localhost\exports\hearnsa.pst
+New-MailboxExportRequest -ContentFilter {(Received -ge '05/01/2017')} -Mailbox hearnsa -FilePath \\localhost\exports\hearnsa.pst
+#>
